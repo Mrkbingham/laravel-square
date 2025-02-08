01826df18ce9dc48f6bf5921785d810c7bd962c7
mbingham <mark.bingham@interworks.com>
2025-02-08 17:27:50 -0600

    adds refund table

---
+++ src/database/migrations/2025_02_08_223051_create_nikoklag_order_refund_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Square\Models\RefundStatus;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nikolag_order_refunds', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('refundable');
+            $table->integer('quantity')->default(1);
+            $table->string('reason', 192);
+            $table->enum('status', [
+                RefundStatus::PENDING,
+                RefundStatus::APPROVED,
+                RefundStatus::REJECTED,
+                RefundStatus::FAILED,
+            ])->default(RefundStatus::PENDING);
+            // Relationships
+            // $table->unsignedBigInteger('transaction_id')->nullable();
+            // $table->unsignedBigInteger('tender_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nikolag_order_refunds');
+    }
+};
