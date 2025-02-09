6e06019eb3dc44203883935023c3c8a9e5f57c73
mbingham <mark.bingham@interworks.com>
2025-02-08 18:11:05 -0600

    adds make test

---
+++ tests/unit/RefundTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikola
+ * Date: 6/20/18
+ * Time: 02:33.
+ */
+
+namespace Nikolag\Square\Tests\Unit;
+
+use Nikolag\Square\Exceptions\MissingPropertyException;
+use Nikolag\Square\Facades\Square;
+use Nikolag\Square\Models\OrderRefundPivot;
+use Nikolag\Square\Models\Product;
+use Nikolag\Square\Models\Refund;
+use Nikolag\Square\Models\Tax;
+use Nikolag\Square\Tests\Models\Order;
+use Nikolag\Square\Tests\TestCase;
+use Nikolag\Square\Utils\Constants;
+
+class RefundTest extends TestCase
+{
+    /**
+     * Refund creation.
+     *
+     * @return void
+     */
+    public function test_refund_make(): void
+    {
+        $product = factory(Product::class)->make();
+        $refund  = factory(Refund::class)->make([
+            'refundable_id' => $product->id,
+            'refundable_type' => Constants::PRODUCT_NAMESPACE,
+            'quantity' => 1,
+        ]);
+
+        $this->assertNotNull($refund, 'Refund is null.');
+    }
+}
