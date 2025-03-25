911cf737a0da66575f76591e969e31d3d8f8ce7c
mbingham <mark.bingham@interworks.com>
2025-03-25 08:52:53 -0500

    increases note column length

---
+++ src/database/migrations/2025_03_25_124239_increases_product_note_string_length.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Square\Models\CatalogModifierListSelectionType;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('nikolag_products', function (Blueprint $table) {
+            // Increase the note column length
+            $table->string('note', 2000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('nikolag_products', function (Blueprint $table) {
+            $table->string('note', 50)->change();
+        });
+    }
+};
